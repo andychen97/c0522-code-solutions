34b6571ec8d738b9e67654b27591bffefa997e91
Andy Chen <chen.andy18@gmail.com>
2022-05-16 19:53:46 +0000

    completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 20;
+var area = width * height;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 200;
+var payment = 12000;
+var change = payment - bill;
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 100;
+var midterm = 90;
+var final = 80;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('type of grade: ', typeof grade);
+
+var firstName = 'Andy';
+var lastName = 'Chen';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 10;
+var isAcidic = pH < 7;
+console.log('value of isAcidic :', isAcidic);
+console.log('type of isAcidic: ', typeof isAcidic);
+
+var headCount = 500;
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
