70f23695d39ea0fb80962397b8e8c26e68ccc586
Andy Chen <chen.andy18@gmail.com>
2022-05-17 18:35:08 +0000

    added authors of books per github comment and also added deliveryMessage.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -5,8 +5,10 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: '114-3941689-8772232',
     deliveredDate: 'August 08, 2020',
+    deliveryMessage: null,
     items: [{
       item: 'Javascript for impatient programmers',
+      author: 'Rauschmayer, Dr. Axel',
       returnWindow: 'September 07, 2020',
       subtotal: 31.55
     }]
@@ -17,8 +19,10 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: '113-9984268-1280257',
     deliveredDate: 'July 20, 2020',
+    deliveryMessage: 'Your package was delivered. It was handed directly to a resident.',
     items: [{
       item: 'The Timeless Way of Building',
+      author: 'Alexander, Christopher',
       returnWindow: 'August 19, 2020',
       subtotal: 41.33
     }]
@@ -29,8 +33,10 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: '114-2875557-9059409',
     deliveredDate: 'July 07, 2020',
+    deliveryMessage: 'Your package was delivered. It was handed directly to a resident.',
     items: [{
-      item: 'Gamecube Controller Adapter.',
+      item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Drive and No Lag-Gamecube Adapter',
+      author: null,
       returnWindow: 'August 05, 2020',
       subtotal: 15.98
     }]
@@ -41,13 +47,16 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: '113-2883177-2648248',
     deliveredDate: 'July 05, 2020',
+    deliveryMessage: null,
     items: [{
       item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      author: null,
       returnWindow: 'August 04, 2020',
       subtotal: 94.95
     },
     {
-      item: 'Javascript for impatient programmers',
+      item: 'The Art of Sql',
+      author: 'Faroult, Stephane',
       returnWindow: 'August 04, 2020',
       subtotal: 33.99
     }]
