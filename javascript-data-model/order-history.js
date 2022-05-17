e29931f8889f39c782d03e14a37de02aefb24cb4
Andy Chen <chen.andy18@gmail.com>
2022-05-17 18:25:05 +0000

    completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,55 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 04, 2020',
+    orderTotal: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveredDate: 'August 08, 2020',
+    items: [{
+      item: 'Javascript for impatient programmers',
+      returnWindow: 'September 07, 2020',
+      subtotal: 31.55
+    }]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    orderTotal: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveredDate: 'July 20, 2020',
+    items: [{
+      item: 'The Timeless Way of Building',
+      returnWindow: 'August 19, 2020',
+      subtotal: 41.33
+    }]
+  },
+  {
+    orderPlaced: 'July 04, 2020',
+    orderTotal: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveredDate: 'July 07, 2020',
+    items: [{
+      item: 'Gamecube Controller Adapter.',
+      returnWindow: 'August 05, 2020',
+      subtotal: 15.98
+    }]
+  },
+  {
+    orderPlaced: 'July 03, 2020',
+    orderTotal: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveredDate: 'July 05, 2020',
+    items: [{
+      item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      returnWindow: 'August 04, 2020',
+      subtotal: 94.95
+    },
+    {
+      item: 'Javascript for impatient programmers',
+      returnWindow: 'August 04, 2020',
+      subtotal: 33.99
+    }]
+  }
+];
