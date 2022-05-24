a981542f6adcd400ac058805cc82f485883fcd94
Andy Chen <chen.andy18@gmail.com>
2022-05-24 22:54:12 +0000

    removed counter and revised to boolean.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,14 +1,11 @@
-var clickCount = 0;
 var $circle = document.querySelector('.circle');
 var $background = document.querySelector('body');
 $circle.addEventListener('click', function () {
-  if (clickCount % 2 === 0) {
-    $circle.className = 'col-1 black-circle';
-    $background.className = 'dark-background';
-    clickCount++;
-  } else {
+  if ($circle.className === 'col-1 black-circle') {
     $circle.className = 'col-1 circle';
     $background.className = 'light-background';
-    clickCount++;
+  } else {
+    $circle.className = 'col-1 black-circle';
+    $background.className = 'dark-background';
   }
 });
