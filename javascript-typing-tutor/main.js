8295efce0c2153dd0ccc1f943616e54c2340e13a
Andy Chen <chen.andy18@gmail.com>
2022-05-25 23:42:20 +0000

    completed javascript-typing-tutor.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,29 +1,11 @@
-var $h1 = document.querySelector('h1');
-var $text = $h1.textContent;
-var fullMessage = '';
-for (var i = 0; i < $text.length; i++) {
-  // var wrapped = '<span>' + $text[i] + '</span>';
-  // fullMessage += wrapped;
-}
-
-$h1.innerHTML = fullMessage;
-
-var $span = document.getElementsByTagName('span');
-
-// var $span = document.querySelectorAll('span');
-$h1.addEventListener('click', function (event) {
-  for (var i = 0; i < $h1.length; i++) {
-    if (event === $h1[i].textContent) {
-      $span[i].className = 'green-text';
-    }
+var count = 0;
+var $span = document.querySelectorAll('span');
+document.addEventListener('keydown', function (event) {
+  if (event.key === $span[count].textContent) {
+    $span[count].className = 'green-text';
+    $span[count + 1].className = 'underscore';
+    count++;
+  } else {
+    $span[count].className = 'underscore red-text';
   }
 });
-
-// var $h1Split = $h1.innerHTML.split('');
-// for (var i = 0; i < $h1Split.length; i++) {
-//   var $span = document.createElement('span');
-//   $h1.appendChild($span);
-//   $span.innertext = $h1Split[i];
-// }
-
-// console.log($h1Split[0]);
