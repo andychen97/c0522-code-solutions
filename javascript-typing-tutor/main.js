0d9236e3675615849c4c17bd79a03b70f93595f2
Andy Chen <chen.andy18@gmail.com>
2022-05-25 17:05:51 +0000

    in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,12 +2,28 @@ var $h1 = document.querySelector('h1');
 var $text = $h1.textContent;
 var fullMessage = '';
 for (var i = 0; i < $text.length; i++) {
-  var wrapped = '<span>' + $text[i] + '</span>';
-  fullMessage += wrapped;
+  // var wrapped = '<span>' + $text[i] + '</span>';
+  // fullMessage += wrapped;
 }
 
 $h1.innerHTML = fullMessage;
 
-var $span = document.querySelectorAll('span');
-$span.addEventListener('keydown', function (event) {
+var $span = document.getElementsByTagName('span');
+
+// var $span = document.querySelectorAll('span');
+$h1.addEventListener('click', function (event) {
+  for (var i = 0; i < $h1.length; i++) {
+    if (event === $h1[i].textContent) {
+      $span[i].className = 'green-text';
+    }
+  }
 });
+
+// var $h1Split = $h1.innerHTML.split('');
+// for (var i = 0; i < $h1Split.length; i++) {
+//   var $span = document.createElement('span');
+//   $h1.appendChild($span);
+//   $span.innertext = $h1Split[i];
+// }
+
+// console.log($h1Split[0]);
