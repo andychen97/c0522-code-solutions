15bdd4f15bba55a330fc878d5eee3ef34a81d9fc
Andy Chen <chen.andy18@gmail.com>
2022-05-26 23:46:18 +0000

    completed javascript-view-swapping.

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,23 @@
+var $tabContainer = document.querySelector('.tab-container');
+var $tab = document.querySelectorAll('.tab');
+var $view = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', function (event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < $tab.length; i++) {
+      if ($tab[i] === event.target) {
+        $tab[i].className = 'tab active';
+        var attribute = event.target.getAttribute('data-view');
+        for (var k = 0; k < $view.length; k++) {
+          if ($view[k].getAttribute('data-view') === attribute) {
+            $view[k].className = 'view';
+          } else {
+            $view[k].className = 'view hidden';
+          }
+        }
+      } else {
+        $tab[i].className = 'tab';
+      }
+    }
+  }
+});
