7831a5212ebb29f3ae5ea417c6a4bc83fe6b63f5
Andy Chen <chen.andy18@gmail.com>
2022-05-26 23:58:57 +0000

    revised for loop as per github request.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -8,15 +8,15 @@ $tabContainer.addEventListener('click', function (event) {
       if ($tab[i] === event.target) {
         $tab[i].className = 'tab active';
         var attribute = event.target.getAttribute('data-view');
-        for (var k = 0; k < $view.length; k++) {
-          if ($view[k].getAttribute('data-view') === attribute) {
-            $view[k].className = 'view';
-          } else {
-            $view[k].className = 'view hidden';
-          }
-        }
       } else {
         $tab[i].className = 'tab';
+      }
+    }
+    for (var k = 0; k < $view.length; k++) {
+      if ($view[k].getAttribute('data-view') === attribute) {
+        $view[k].className = 'view';
+      } else {
+        $view[k].className = 'view hidden';
       }
     }
   }
