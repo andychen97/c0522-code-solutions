0469454ef9b510835c22cf1923b83407187bca18
Andy Chen <chen.andy18@gmail.com>
2022-05-27 19:57:57 +0000

    completed javascript-and-json.

---
+++ javascript-and-json/main.js
@@ -0,0 +1,28 @@
+var books = [
+  {
+    isbn: '234234-435',
+    title: 'book one',
+    author: 'first person'
+  },
+  {
+    isbn: '4355645-664',
+    title: 'book two',
+    author: 'second person'
+  },
+  {
+    isbn: '234234-435',
+    title: 'book three',
+    author: 'third person'
+  }
+];
+
+console.log('value of books:', books);
+console.log('typeof books:', typeof books);
+
+var bookStrings = JSON.stringify(books);
+console.log('typeof JSON.stringify books:', typeof bookStrings);
+
+var string = '{"id": 1, "name": "Andy Chen"}';
+console.log('typeof JSON string:', typeof string);
+var parseJSON = JSON.parse(string);
+console.log('typeof parseJSON:', parseJSON);
