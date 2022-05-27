e8dec95778475ea114f71e93d499cf5bf29a6c30
Andy Chen <chen.andy18@gmail.com>
2022-05-27 18:38:08 +0000

    in progress.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,21 @@
 /* exported omit */
+
+/*
+ - define a function with 2 parameters (source, keys)
+ - create an empty object and assign it to a new variable
+ - use a for loop to check if key[i] is in source
+ - add property and value that are not in the array to empty object
+ - return object
+*/
+
+function omit(source, keys) {
+  var obj = {};
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      if (source[keys[i]] !== keys[i]) {
+        obj[keys[i]] = source[keys[i]];
+      }
+    }
+  }
+  return obj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,5 +1,13 @@
 /* exported pick */
 
+/*
+ - define a function with 2 parameters (source, keys)
+ - create an empty object and assign it to a new variable
+ - use a for loop to check if key[i] is in source
+ - add property and value to empty object
+ - return object
+*/
+
 function pick(source, keys) {
   var obj = {};
   for (var i = 0; i < keys.length; i++) {
