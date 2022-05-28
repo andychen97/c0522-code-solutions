d6949dc7175abaf5371105c5b26198552e2e23e2
Andy Chen <chen.andy18@gmail.com>
2022-05-28 00:16:09 +0000

    in progress.

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,12 @@
 /* exported invert */
+
+function invert(source) {
+  var obj = {};
+
+  for (var i = 0; i < source.length; i++) {
+    var key = source[i];
+    var value = source[i[i]];
+    obj[key] = value;
+  }
+  return obj;
+}
