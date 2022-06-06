0ab8481324b16c53012b98ba393a17d3f3486bde
Andy Chen <chen.andy18@gmail.com>
2022-06-06 19:33:46 +0000

    in progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,38 +1,51 @@
 /* exported titleCase */
 
-function titleCase(title) {
-  // var emptyArray = [];
-  // var array = title.toLowerCase().split(' ');
-  var noCap = ['a', 'an', 'and', 'at', 'but', 'by', 'for', 'in', 'nor', 'of', 'on', 'or', 'so', 'the', 'to', 'up', 'yet'];
-  // var acro =
-  // for (var i = 0; i < array.length; i++) {
-  //   if (!(array[i].includes(noCap))) {
-  //     emptyArray.push(array[i].charAt(0).toUpperCase() + array[i].slice(1));
-  //   }
-  // }
-  // return emptyArray.join(' ');
-  var array = title.toLowerCase().split(' ');
-  array[0].charAt(0).toUpperCase();
-  for (var i = 0; i < array.length; i++) {
-    // Use function that checks if array[i] equals to one of the words in noCap
-    if (!(array[i] in noCap)) {
-      var word = array[i];
-      if (word.includes('-')) {
-        word[0].toUpperCase();
-        for (var k = 1; k < word.length; k++) {
-          if (word[k - 1] === '-') {
-            word[i].toUpperCase();
-          }
-        }
-        array[i] = word;
-      } else {
-        array[i].charAt(0).toUpperCase();
-      }
-    }
-  }
+// function titleCase(string) {
+//   var noCap = ['a', 'an', 'and', 'at', 'but', 'by', 'for', 'in', 'nor', 'of', 'on', 'or', 'so', 'the', 'to', 'up', 'yet'];
+//   var lowered = string.toLowerCase().split(' ');
+//   lowered[0] = lowered[0].charAt(0).toUpperCase() + lowered[0].slice(1);
 
-  return array.join(' ');
-}
+//   for (var i = 0; i < lowered.length; i++) {
+//     for (var word = 0; word < lowered[i].length; word++) {
+//       if (lowered[i][word - 1] === '-') {
+//         lowered[i][word] = lowered[i].charAt(lowered[i][word]).toUpperCase() + lowered[i].slice(lowered[i][word + 1]);
+//       }
+//       console.log(word);
+//     }
+
+//     // lowered[i] = lowered[i].charAt(0).toUpperCase() + lowered[i].slice(1);
+//   }
+//   console.log(lowered);
+
+// for (var i = 0; i < string.length; i++) {
+//   if (string[i - 1] === '-') {
+//     var alteredString = newString.slice(0, i) + newString.charAt(i).toUpperCase() + newString.slice(i + 1);
+//   }
+// }
+// }
+
+//   var array = title.toLowerCase().split(' ');
+//   array[0].charAt(0).toUpperCase();
+//   for (var i = 0; i < array.length; i++) {
+//     // Use function that checks if array[i] equals to one of the words in noCap
+//     if (!(array[i] in noCap)) {
+//       var word = array[i];
+//       if (word.includes('-')) {
+//         word[0].toUpperCase();
+//         for (var k = 1; k < word.length; k++) {
+//           if (word[k - 1] === '-') {
+//             word[i].toUpperCase();
+//           }
+//         }
+//         array[i] = word;
+//       } else {
+//         array[i].charAt(0).toUpperCase();
+//       }
+//     }
+//   }
+
+//   return array.join(' ');
+// }
 
 // [Speaking,Javascript:,an,in-depth,guide,for,programmers]
 
