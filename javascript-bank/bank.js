c9a71f4952e352b3a74ea37da1206472b2d61744
Andy Chen <chen.andy18@gmail.com>
2022-06-08 19:00:24 +0000

    in progress.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -11,9 +11,37 @@ Bank.prototype.openAccount = function (holder, balance) {
     var newAccount = new Account(this.nextAccountNumber, this.holder);
     newAccount.deposit(balance);
     this.accounts.push(newAccount);
-    this.nextAccountNumber++;
-    return this.number;
+    return this.nextAccountNumber++;
   } else {
     return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i].holder;
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  if (this.accounts.length === 0) {
+    return 0;
+  } else {
+    var depositHistory = 0;
+    var withdrawHistory = 0;
+    for (var i = 0; i < this.accounts.length; i++) {
+      for (var j = 0; j < this.transactions.length; j++) {
+        if (this.accounts[i].transactions[j].type === 'deposit') {
+          depositHistory += this.accounts[i].transactions[j].amount;
+        } else if (this.accounts[i].transactions[i].type === 'withdrawal') {
+          withdrawHistory += this.accounts[i].transactions[j].amount;
+        }
+      }
+    }
+    var balance = depositHistory - withdrawHistory;
+    return balance;
   }
 };
