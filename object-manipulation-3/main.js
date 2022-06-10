ad99c88b4acc3e8d22106e4963982c5d8cad5b94
Andy Chen <chen.andy18@gmail.com>
2022-06-10 00:46:30 +0000

    in progress.

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,32 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+// var cards = [
+//   { Diamond2: 2 }, { Club2: 2 }, { Heart2: 2 }, { Space2: 2 },
+//   { Diamond3: 3 }, { Club3: 3 }, { Heart3: 3 }, { Space3: 3 },
+//   { Diamond5: 4 }, { Club4: 4 }, { Heart4: 4 }, { Space4: 4 },
+//   { Diamond5: 5 }, { Club5: 5 }, { Heart5: 5 }, { Space5: 5 },
+//   { Diamond6: 6 }, { Club6: 6 }, { Heart6: 6 }, { Space6: 6 },
+//   { Diamond7: 7 }, { Club7: 7 }, { Heart7: 7 }, { Space7: 7 },
+//   { Diamond8: 8 }, { Club8: 8 }, { Heart8: 8 }, { Space8: 8 },
+//   { Diamond9: 9 }, { Club9: 9 }, { Heart9: 9 }, { Space9: 9 },
+//   { Diamond10: 10 }, { Club10: 10 }, { Heart10: 10 }, { Space10: 10 },
+//   { DiamondJ: 10 }, { ClubJ: 10 }, { HeartJ: 10 }, { SpaceJ: 10 },
+//   { DiamondQ: 10 }, { ClubQ: 10 }, { HeartQ: 10 }, { SpaceQ: 10 },
+//   { DiamondK: 10 }, { ClubK: 10 }, { HeartK: 10 }, { SpaceK: 10 },
+//   { DiamondAce: 11 }, { ClubAce: 11 }, { HeartAce: 11 }, { SpaceAce: 11 }
+// ];
+
+// var players = [
+//   { player: 'Andy', card1: null, card2: null },
+//   { player: 'Fred', card1: null, card2: null },
+//   { player: 'Sharon', card1: null, card2: null },
+//   { player: 'Brian', card1: null, card2: null }
+// ];
+
+// function cardGame() {
+//   var playerScore1 = 0;
+//   var playerScore2 = 0;
+//   var playerScore3 = 0;
+//   var playerScore4 = 0;
+
+// }
