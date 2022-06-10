8fe46be77e00ef3aa9b26393309030c5595e60cf
Andy Chen <chen.andy18@gmail.com>
2022-06-10 19:56:41 +0000

    completed javascript-ajax.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,15 @@
+var $userData = document.querySelector('ul');
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function (event) {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (var i = 0; i < xhr.response.length; i++) {
+    var $li = document.createElement('li');
+    $li.textContent = xhr.response[i].name;
+    $userData.appendChild($li);
+  }
+});
+xhr.send();
