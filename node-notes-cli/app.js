fac4cb7106788a9e4a1ec87f8bbdf9da92551401
Andy Chen <chen.andy18@gmail.com>
2022-07-01 00:32:39 +0000

    compressed code.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,39 +1,24 @@
-const process = require('process');
 const fs = require('fs');
 const dataJSON = require('./data.json');
 
-const action = process.argv[2];
-const thirdArg = process.argv[3];
-const update = process.argv[4];
+const stringifyCheckError = () => {
+  const newObj = JSON.stringify(dataJSON, null, 2);
+  fs.writeFile('./data.json', newObj, err => {
+    if (err) throw err;
+  });
+};
 
-if (action === 'read') {
+if (process.argv[2] === 'read') {
   for (var key in dataJSON.notes) {
     console.log(`${key}: ${dataJSON.notes[key]}`);
   }
-} else if (action === 'create') {
-  dataJSON.notes[dataJSON.nextId++] = thirdArg;
-  const newObj = JSON.stringify(dataJSON, null, 2);
-  fs.writeFile('./data.json', newObj, err => {
-    if (err) throw err;
-  });
-} else if (action === 'delete') {
-  for (const key in dataJSON.notes) {
-    if (key === thirdArg.toString()) {
-      delete dataJSON.notes[thirdArg];
-    }
-  }
-  const newObj = JSON.stringify(dataJSON, null, 2);
-  fs.writeFile('./data.json', newObj, err => {
-    if (err) throw err;
-  });
-} else if (action === 'update') {
-  for (const key in dataJSON.notes) {
-    if (key === thirdArg.toString()) {
-      dataJSON.notes[key] = update;
-    }
-  }
-  const newObj = JSON.stringify(dataJSON, null, 2);
-  fs.writeFile('./data.json', newObj, err => {
-    if (err) throw err;
-  });
+} else if (process.argv[2] === 'create') {
+  dataJSON.notes[dataJSON.nextId++] = process.argv[3];
+  stringifyCheckError();
+} else if (process.argv[2] === 'delete') {
+  delete dataJSON.notes[process.argv[3]];
+  stringifyCheckError();
+} else if (process.argv[2] === 'update') {
+  dataJSON.notes[process.argv[3]] = process.argv[4];
+  stringifyCheckError();
 }
