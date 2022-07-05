d0faaef29aac05e5d490fa1f9711f603a3e2d1df
Andy Chen <chen.andy18@gmail.com>
2022-07-05 21:48:49 +0000

    app.post completed. app.delete in progress.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const dataJSON = require('./data.json');
 
 app.get('/api/notes', (req, res) => {
@@ -24,6 +25,28 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(404).send(message404);
   } else {
     res.status(200).send(dataJSON.notes[id]);
+  }
+});
+
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+  if (!(req.body.content)) {
+    const message400 = { error: 'content is a required field.' };
+    res.status(400).send(message400);
+  } else if (req.body.content !== null) {
+    const newContent = req.body;
+    const id = dataJSON.nextId++;
+    newContent.id = id;
+    dataJSON.notes[id] = newContent;
+    res.status(201).send(dataJSON.notes[id]);
+    const newObj = JSON.stringify(dataJSON, null, 2);
+    fs.writeFile('./data.json', newObj, err => {
+      if (err) throw err;
+    });
+  } else {
+    const message500 = { error: 'An unexpected error occured.' };
+    res.status(500).send(message500);
   }
 });
 
