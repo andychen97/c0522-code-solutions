c4a30cf6c3860711aa179cafc57777047db10df5
Andy Chen <chen.andy18@gmail.com>
2022-07-05 18:03:01 +0000

    in progress for revision.

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -2,25 +2,34 @@ const express = require('express');
 const app = express();
 
 let nextId = 1;
-let grades = {};
-const newArray = [];
+const grades = {};
 
 app.get('/api/grades', (req, res) => {
+  const newArray = [];
+  for (const id in grades) {
+    newArray.push(grades[id]);
+  }
   res.json(newArray);
 });
 
 app.use(express.json());
 
 app.post('/api/grades', (req, res) => {
+  const newGrade = req.body;
+  const id = nextId++;
+  newGrade.id = id;
+  grades[id] = newGrade;
+  res.status(201).json(newGrade);
 
-  grades = {
-    id: nextId++,
-    name: req.body.name,
-    course: req.body.course,
-    score: req.body.score
-  };
-  newArray.push(grades);
-  res.status(201).send(grades);
+  // MY SOLUTION
+  // const newGrades = {
+  //   id: nextId++,
+  //   name: req.body.name,
+  //   course: req.body.course,
+  //   score: req.body.score
+  // };
+  // grades.id = newGrades;
+  // res.status(201).send(newGrades);
 });
 
 app.listen(3000, () => {
