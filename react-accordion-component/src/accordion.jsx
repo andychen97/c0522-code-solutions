46ab25bc2a77a384693631bdb8c46d3dacd3e892
Andy Chen <chen.andy18@gmail.com>
2022-07-22 05:22:04 +0000

    in progress.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,68 +1,42 @@
 import React from 'react';
 
 export default class Accordion extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tab: 0
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     tab: 0
+  //   };
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
 
-  handleClick(table) {
+  // handleClick() {
 
-  }
+  // }
 
   render() {
-    const tab = this.state.tab;
-    const handleClick = this.handleClick;
-    let view;
-    if (tab === 0) {
-      view = 'hidden';
-    }
+    const languages = [
+      { name: 'Hypertext Markup Language', description: 'Hypertext Markup Language (HTML) is the standard markup Language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.' },
+      { name: 'Cascading Style Sheet', description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML.CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript.' },
+      { name: 'JavaScript', description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification.JavaScript has curly- bracket syntax, dynamic typing, prototype - based object - orientation and first class functions.' }
+    ];
+    const listedItems = languages.map(({ name, description }) => {
+      return (
+        <li key={name}>
+          <div className='row'>
+            <div className='box'>
+              <h3>{name}</h3>
+            </div>
+          </div>
+          <div className='row'>
+            <div className="descript-box">
+              <p>{description}</p>
+            </div>
+          </div>
+        </li>
+      );
+    });
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='box' onClick={handleClick}>
-            <h3>Hypertext Markup Language</h3>
-          </div>
-        </div>
-        <div className='row'>
-          <div className={`descript-box ${view}`}>
-            <p>Hypertext Markup Language (HTML) is the standard markup Language
-              for creating web pages and web applications. With Cascading Style
-              Sheets (CSS) and JavaScript, it forms a triad of cornerstone
-              technologies for the World Wide Web.</p>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='box' onClick={handleClick}>
-            <h3>Cascading Style Sheet</h3>
-          </div>
-        </div>
-        <div className='row'>
-          <div className={`descript-box ${view}`}>
-            <p>Cascading Style Sheets (CSS) is a style sheet language used for
-              describing the presentation of a document written in a markup
-              language like HTML. CSS is a cornerstone technology of the World
-              Wide Web alongside HTML and JavaScript</p>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='box' onClick={handleClick}>
-            <h3>JavaScript</h3>
-          </div>
-        </div>
-        <div className='row'>
-          <div className={`descript-box ${view}`}>
-            <p>JavaScript, often abbreviated as JS, is a high-level,
-              nterpreted programming language that conforms to the ECMAScript
-              specification. JavaScript has curly-bracket syntax, dynamic
-              typing, prototype-based object-orientation and first class
-              functions.</p>
-          </div>
-        </div>
-      </div>
+      <ul>{listedItems}</ul>
     );
   }
 }
