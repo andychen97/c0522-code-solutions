cc297900c6b0b9b8699374baaead7127cc68c53f
Andy Chen <chen.andy18@gmail.com>
2022-08-09 00:47:57 +0000

    revised answers.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -1,20 +1,20 @@
 /* exported uniqueQuadratic, uniqueLinear */
 
 function uniqueLinear(words) {
-  const seen = {};                  // 1 * 1 = O(1) ~= 0(1)
+  const seen = {};                  // 1 * 1 = O(1) ~= 0(1) //constant time
   const unique = [];                // 1 * 1 = O(1) ~= 0(1)
   for (
     let i = 0;                      // 1 * 1 = O(1) ~= 0(1)
-    i < words.length;               // 2 * n = O(2n) ~= 0(n)
+    i < words.length;               // 2 * n = O(2n) ~= 0(n) // linear time
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
     const word = words[i];          // 2 * n = O(2n) ~= 0(n)
     if (!seen[word]) {              // 3 * n = O(3n) ~= 0(n)
-      seen[word] = true;
+      seen[word] = true;            // 2 * n = 0(2n) ~= 0(n)
       unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
     }
   }
-  return unique;
+  return unique;                    // 1 * 1 = 0(1) ~= 0(1)
 } // Big O Notation for uniqueLinear: 0(n)
 
 function uniqueQuadratic(words) {
@@ -24,15 +24,15 @@ function uniqueQuadratic(words) {
     i < words.length;               // 2 * n = O(2n) ~= 0(n)
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
-    const word = words[i];          // 3 * n = O(3n) ~= 0(n)
-    let isUnique = true;            // 1 * 1 = O(1) ~= O(1)
+    const word = words[i];          // 2 * n = O(2n) ~= 0(n)
+    let isUnique = true;            // 1 * n = O(n) ~= O(n)
     for (
-      let c = 0;                    // 1 * n^2 = O(n^2) ~= O(n^2)
-      c < i;                        // 2 * n^2 = 0(2n^2) ~= 0(n^2)
+      let c = 0;                    // 1 * n = O(n) ~= O(n)
+      c < i;                        // 1 * n^2 = 0(n^2) ~= 0(n^2) //quadratic time
       c++                           // 2 * n^2 = O(2n^2) ~= 0(n^2)
     ) {
-      const comparing = words[c];   // 3 * n^2 = O(3n^2) ~= 0(n^2)
-      if (comparing === word) {     // 3 * n^2 = O(3n^2) ~= 0(n^2)
+      const comparing = words[c];   // 2 * n^2 = O(2n^2) ~= 0(n^2)
+      if (comparing === word) {     // 2 * n^2 = O(3n^2) ~= 0(n^2)
         isUnique = false;           // 1 * n^2 = O(n^2) ~= 0(n^2)
       }
     }
