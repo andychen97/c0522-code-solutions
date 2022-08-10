3c1d04ef676a6a81724b91d119333a1b02aa7876
Andy Chen <chen.andy18@gmail.com>
2022-08-10 23:30:02 +0000

    in progress.

--- data-structures-linked-lists/append.js
+++ data-structures-linked-lists/append.js
@@ -2,4 +2,12 @@
 /* exported append */
 
 function append(list, value) {
+  let current = list;
+  let next = list.next;
+  while (next !== null) {
+    current = next;
+    next = current.next;
+  }
+
+  return current;
 }
